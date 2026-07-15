097dcb3dddeea62022b79444d0937df813ebe794
Rachna1999-Rajak <rachnarajak040@gmail.com>
2026-07-15 16:12:04 +0530

    jenkins file

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -58,6 +58,7 @@ export default function CartPage() {
                 className="btn-primary"
                 onClick={() => router.push("/restaurants")}
               >
+                
                 Browse Restaurants
               </button>
             </div>
